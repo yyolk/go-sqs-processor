2bc2fea05cd872dd9eacc2dd2beb84e681b076d2
Joseph Chiocchi <input.output@gmail.com>
2017-05-29 21:15:22 -0500

    init go-sqs-processor proto

---
+++ processor.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"os"
+)
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
+
+func main() {
+
+	var name string
+	var timeout int64
+	flag.StringVar(&name, "n", "", "Queue name")
+	flag.Int64Var(&timeout, "t", 20, "(Optional) Timeout in seconds for long polling")
+	flag.Parse()
+
+	if len(name) == 0 {
+		flag.PrintDefaults()
+		exitErrorf("Queue name required")
+	}
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create a SQS service client.
+	svc := sqs.New(sess)
+
+	resultURL, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
+		QueueName: aws.String(name),
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == sqs.ErrCodeQueueDoesNotExist {
+			exitErrorf("Unable to find queue %q.", name)
+		}
+		exitErrorf("Unable to queue %q, %v.", name, err)
+	}
+
+	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		QueueUrl: resultURL.QueueUrl,
+		AttributeNames: aws.StringSlice([]string{
+			"SentTimestamp",
+		}),
+		MaxNumberOfMessages: aws.Int64(1),
+		MessageAttributeNames: aws.StringSlice([]string{
+			"All",
+		}),
+		WaitTimeSeconds: aws.Int64(timeout),
+	})
+	if err != nil {
+		exitErrorf("Unable to receive message from queue %q, %v.", name, err)
+	}
+
+	fmt.Printf("Received %d messages.\n", len(result.Messages))
+	if len(result.Messages) > 0 {
+		fmt.Println(result.Messages)
+	}
+	fmt.Println("vim-go")
+}
